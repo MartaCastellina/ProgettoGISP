b45ac1d0e8c93d32f6c473229857ac35dcffb63b
Marta <Marta@LAPTOP-OM1PLPTE>
2020-04-24 16:33:54 +0200

    Terzo Commit

---
+++ GispICT/src/main/java/it/polito/tdp/GispICT/Reparto.java
@@ -0,0 +1,20 @@
+package it.polito.tdp.GispICT;
+
+public class Reparto {
+	private String Nome;
+	private int RID;
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public int getRID() {
+		return RID;
+	}
+	public void setRID(int rID) {
+		RID = rID;
+	}
+	
+
+}
