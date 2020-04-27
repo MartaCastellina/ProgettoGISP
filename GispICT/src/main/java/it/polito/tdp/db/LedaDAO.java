c3dc671b7c9bd683b1cdb4e60dea4b460bbb7aaa
LedaVellucci <leda.vellucci@studenti.polito.it>
2020-04-27 11:53:27 +0200

    Add files via upload

--- GispICT/src/main/java/it/polito/tdp/db/LedaDAO.java
+++ GispICT/src/main/java/it/polito/tdp/db/LedaDAO.java
@@ -1,5 +1,35 @@
-package it.polito.tdp.db;
-
+package it.polito.tdp.GispICT;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 public class LedaDAO {
 
-}
+		public List<FarmacoNelReparto>listaReparti(int codice){
+			String sql = "SELECT IDWard,NameWard,Quantity, NamePharma, ExpDate FROM magazzinoreparti WHERE IDPharma="+codice;
+			
+			List<FarmacoNelReparto> result = new ArrayList<FarmacoNelReparto>();
+			
+			Connection conn = DBConnection.getConnection() ;
+			
+			try {
+				PreparedStatement st = conn.prepareStatement(sql) ;
+				
+				ResultSet res = st.executeQuery() ;
+				
+				while(res.next()) {
+					result.add( new FarmacoNelReparto(res.getString("NamePharma"), res.getDate("ExpDate"),res.getInt(codice), res.getInt("Quantity"), res.getString("NameWard"), res.getInt("IDWard"))) ;
+				}
+				
+				conn.close();
+				return result ;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return null ;
+			}
+		}
+	}
