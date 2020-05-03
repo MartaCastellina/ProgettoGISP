24a6b55f3f0f9f2e372e363a58d2704e40bd4953
LedaVellucci <leda.vellucci@studenti.polito.it>
2020-05-03 20:14:28 +0200

    Add files via upload

--- GispICT/src/main/java/it/polito/tdp/db/LedaDAO.java
+++ GispICT/src/main/java/it/polito/tdp/db/LedaDAO.java
@@ -1,17 +1,16 @@
-package it.polito.tdp.db;
+package it.polito.tdp.GispICT;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
-import it.polito.tdp.GispICT.FarmacoNelReparto;
 public class LedaDAO {
 
-		public List<FarmacoNelReparto>listaReparti(int codice){
-			String sql = "SELECT IDWard,NameWard,Quantity, NamePharma, ExpDate FROM magazzinoreparti WHERE IDPharma="+codice;
+		public List<FarmacoNelReparto>listaReparti(String nome){
+			String sql = "SELECT IDWard,NameWard,Quantity, NamePharma,IDPharma,ExpDate FROM magazzinoreparti WHERE NamePharma LIKE '%"+nome+"%'";
 			
 			List<FarmacoNelReparto> result = new ArrayList<FarmacoNelReparto>();
 			
@@ -23,7 +22,7 @@ public class LedaDAO {
 				ResultSet res = st.executeQuery() ;
 				
 				while(res.next()) {
-					result.add( new FarmacoNelReparto(res.getString("NamePharma"), res.getDate("ExpDate").toLocalDate(),res.getInt(codice), res.getInt("Quantity"), res.getString("NameWard"), res.getInt("IDWard"))) ;
+					result.add( new FarmacoNelReparto(res.getString("NamePharma"), res.getDate("ExpDate").toLocalDate(),res.getInt("IDPharma"), res.getInt("Quantity"), res.getString("NameWard"), res.getInt("IDWard"))) ;
 				}
 				
 				conn.close();
@@ -34,4 +33,47 @@ public class LedaDAO {
 				return null ;
 			}
 		}
-	}
+		public void decrementa(FarmacoNelReparto f,int q) {
+			int nuova= f.getQuantita()-q;
+			System.out.print(nuova);
+			Date data=java.sql.Date.valueOf(f.getScadenza());
+			if(nuova==0){
+				String sql="DELETE FROM magazzinoreparti WHERE IDWard=? AND IDPharma=? AND ExpDate=?;";
+				Connection conn = DBConnection.getConnection() ;
+			
+			try {
+				PreparedStatement st = conn.prepareStatement(sql) ;
+				st.setInt(1,f.getRID());
+				st.setInt(2,f.getFID());
+				st.setDate(3,(java.sql.Date) data);
+				
+				st.executeUpdate();
+				
+				conn.close();
+				
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	
+				
+			}}
+			else
+			{   
+				String sql="UPDATE magazzinoreparti SET Quantity = ? WHERE IDWard=? AND IDPharma=? AND ExpDate=?;";
+				Connection conn = DBConnection.getConnection() ;
+			
+			try {
+				PreparedStatement st = conn.prepareStatement(sql) ;
+				st.setInt(1,nuova);
+				st.setInt(2,f.getRID());
+				st.setInt(3,f.getFID());
+				st.setDate(4,(java.sql.Date) data);
+				
+				st.executeUpdate();
+				
+				conn.close();
+				
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	}}}}
